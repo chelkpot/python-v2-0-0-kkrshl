4a13563e17d842545cd3e169e3b1a8d601f50f7d
Карина <kkrshl@mail.ru>
2025-10-26 23:26:12 +0500

    sds

--- tasks/task2.py
+++ tasks/task2.py
@@ -3,10 +3,7 @@
 def solve():
 # Ниже пишите решение задачи
 x, y, z = map(int, input().split())
-price_x = 3
-price_y = price_x + 2
-price_z = price_y + 7
-print(price_x * x + price_y * y + price_z * z)
+print(3 * x + 5 * y + 12 * z)
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
