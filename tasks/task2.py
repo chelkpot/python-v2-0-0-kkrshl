22af597411576cac9181200c00f659d4809f5dbf
Карина <kkrshl@mail.ru>
2025-10-26 23:23:15 +0500

    sds

--- tasks/task2.py
+++ tasks/task2.py
@@ -3,14 +3,11 @@
 def solve():
 # Ниже пишите решение задачи
 x, y, z = map(int, input().split())
-    price_pencil = 3
-    price_pen = price_pencil + 2
-    price_marker = price_pen + 7
-    total = x * price_pencil + y * price_pen + z * price_marker
-    print(total)
-   
+price_x = 3
+price_y = price_x + 2
+price_z = price_y + 7
+print(price_x * x + price_y * y + price_z * z)
 
-   
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
