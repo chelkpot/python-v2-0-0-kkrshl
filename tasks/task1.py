8befb72fd6f87825224c166b6086b2c2e1a2e769
Карина <kkrshl@mail.ru>
2025-10-26 16:40:17 +0500

    sds

--- tasks/task1.py
+++ tasks/task1.py
@@ -2,7 +2,10 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
+    S=int(input())
+    petyaserg= S // 6
+    katya= 4 * petyaserg
+    print(petyaserg, katya, petyaserg)
 
     
 # Код ниже не трогать! он нужен для тестов
