d6fb0d18313199d1edc37267d04a79579373b316
Карина <kkrshl@mail.ru>
2025-11-23 16:42:56 +0500

    sts

--- tasks/task2.py
+++ tasks/task2.py
@@ -2,7 +2,7 @@
 
 def solve():
 # Ниже пишите решение задачи
-x, y, z = map(int, input().split())
+    x, y, z = map(int, input().split())
 print(3 * x + 5 * y + 12 * z)
 
 # Код ниже не трогать! он нужен для тестов
